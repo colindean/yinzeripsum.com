d111952e86029a7488aff45a3d230224c7c56bc1
Colin Dean <git@cad.cx>
2012-10-07 22:08:19 -0400

    basic site

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'fillertext'
 
 # Helpers
 require './lib/render_partial'
@@ -12,11 +13,25 @@ set :views, 'views'
 set :public_folder, 'public'
 set :haml, {:format => :html5} # default Haml format is :xhtml
 
+FillerText::FillerText.style = FillerText::Style::YinzerIpsum
+VALID_CHUNKS = [:sentences, :words, :characters, :bytes, :paragraphs]
+
 # Application routes
 get '/' do
   haml :index, :layout => :'layouts/application'
 end
 
-get '/about' do
-  haml :about, :layout => :'layouts/page'
+get '/yinzer' do
+  content_type 'text/plain'
+  what = params[:chunk] || VALID_CHUNKS.first
+  num = params[:num] || 1
+  what = what.to_sym
+  num = num.to_i
+  raise "Invalid chunk specified" if !VALID_CHUNKS.member?(what)
+
+  FillerText::FillerText.send what, num
+end
+
+get '/api' do
+  haml :api, :layout => :'layouts/page'
 end
